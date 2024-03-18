c8ce78d61a564c4ff0162578bb7b74c233c702c7
kaanan <kaanan99@gmail.com>
2024-03-17 20:57:32 -0700

    Added system evaluation code

--- seal_counter/Notebooks/RCNN Notebooks/rcnn_utils.py
+++ seal_counter/Notebooks/RCNN Notebooks/rcnn_utils.py
@@ -1,11 +1,14 @@
 import numpy as np
+import pandas as pd
 import torch
 import torchvision
 
+from bs4 import BeautifulSoup as bs
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights, fasterrcnn_resnet50_fpn_v2
 from torch.utils.data import  Dataset
 from torchvision import transforms
+from typing import List
 
 class SealDataset(Dataset):
     def __init__(self, images, targets, transform=transforms.Compose([transforms.ToTensor()])):
@@ -160,13 +163,81 @@ def predict(model, image, device="cpu", transform = transforms.Compose([transfor
     pred = model(image)[0]
     return pred
 
+
 def detach_pred(pred):
     pred["boxes"] = pred["boxes"].detach().cpu()
     pred["scores"] = pred["scores"].detach().cpu()
     pred["labels"] = pred["labels"].detach().cpu()
     return pred
 
+
 def write_to_latex(df, file_name, long_table=False):
     f = open(f"{file_name}.txt", "w")
     f.write(df.to_latex(index=False, longtable=long_table))
     f.close()
+
+
+def parse_xml(xml) -> pd.DataFrame:
+   """Creates a dataframe from an xml file containing bounding box information for that image
+
+   Args:
+       xml (XML file): The xml file containing bounding box information
+
+   Returns:
+       pd.DataFrame: Information about bounding boxes stored as pandas dataframe
+   """
+   # Find the relevant attributes
+   label = xml.find_all("name")
+   xmin = xml.find_all("xmin")
+   ymin = xml.find_all("ymin")
+   xmax = xml.find_all("xmax")
+   ymax = xml.find_all("ymax")
+
+   # There is sometimes an error where one of the attributes is missing
+   min_size = min(len(label), len(xmin), len(ymin), len(xmax), len(ymax))
+  
+   # Extract text
+   for i in range(min_size):
+      label[i] = label[i].text
+      xmin[i] = xmin[i].text
+      ymin[i] = ymin[i].text
+      xmax[i] = xmax[i].text
+      ymax[i] = ymax[i].text
+      
+   # Create dataframe with all bounding box infomation
+   df = pd.DataFrame(
+      {
+         "label": label[:min_size], 
+         "xmin": xmin[:min_size], 
+         "ymin": ymin[:min_size], 
+         "xmax": xmax[:min_size], 
+         "ymax": ymax[:min_size]}
+     )
+   
+   return df
+
+def get_bb(in_path:str, xml:List[str]) -> pd.DataFrame:
+   """_summary_
+
+   Args:
+       in_path (str): Path to directory containing XML files
+       xml (List[str]): List of XML files to open
+
+   Returns:
+       pd.DataFrame: A dataframe containing bounding box information
+   """
+   df = pd.DataFrame()
+   i = 0
+   
+   for x in xml:
+      f = open(in_path + x)
+      xml_file = bs("".join(f.readlines()), "lxml")
+      
+      df_temp = parse_xml(xml_file)
+      df_temp.insert(0, "file_num", str(i).zfill(4))
+      df = pd.concat([df, df_temp])
+      
+      f.close()
+      
+      i+=1
+   return df
