c6f2b081974c9794daa13bce7e730aebc3cf3bb9
Kaanan Vinay Kharwa <kkharwa@f35.csc.calpoly.edu>
2024-03-22 11:25:37 -0700

    Modified docker start script to mount TrainModels

--- start_docker.sh
+++ start_docker.sh
@@ -2,6 +2,7 @@ docker run \
    --runtime=nvidia \
    -v "/data2/noses_data/seal_counting_thesis_data/Training, Val, and Test Images":"/seal_counting/Training, Val, and Test Images" \
    -v /data2/noses_data/seal_counting_thesis_data/Data:/seal_counting/seal_detector/Data \
+   -v /data2/noses_data/seal_counting_thesis_data/TrainedModels:/seal_counting/TrainedModels \
    -p 8889:8888 \
    -p 8890:6006\
    --name seal_thesis_container \
